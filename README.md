bc601253fcfd175d615b05afd1d36bebf9471bca
Simon Akpoveso <simonakpoveso@Simons-MacBook-Pro.local>
2020-08-15 04:47:00 +0100

    Added functions for communication in accessory mode

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # libusb_comm_cpp
 A usb library using libusb for communication with android devices in accessory mode
 
-
+#Dependencies
+1. libusb-1.0 has to be installed on your system
--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <libusb-1.0/libusb.h>
+#include <Usb.h>
+#include <unistd.h>
+#include "openaccessory.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    OpenAccessory* oa;
+    vector<uint8_t> in;
+    try{
+        OpenAccessory::init();
+        cout << "Lib Loaded" << endl;
+        oa = new OpenAccessory("Softcom", "Moonshot",
+                               "442 Biometric Scanner", "1.0",
+                               "https://www.softcom.ng",
+                               "0123456789", USB_VENDOR_GOOGLE,
+                               USB_PRODUCT_NEXUS_S);
+        cout << "Dev Loaded" << endl;
+    } catch (OpenAccessoryException &e) {
+        cerr << e.what() << endl;
+        cerr << "Exit Test with Failure 1" << endl;
+        OpenAccessory::de_init();
+        exit(-1);
+    }
+
+    try{
+        while(1){
+            oa->read_data(in);
+            cout << in.data() << endl;
+        }
+    }
+    catch(OpenAccessoryException& e){
+        cerr << e.what() << endl;
+        cerr << "Exit Test with Failure" << endl;
+    }
+    delete oa;
+    OpenAccessory::de_init();
 }
